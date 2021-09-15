1a244e6fe894c8f6d221ba57b01d27b1f427c1ff
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-15 17:00:21 -0300

    created Stand class and added to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stand
+
+admin.site.register(Stand)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Stand(models.Model):
+    stand_name = models.CharField(max_length=30)
+    stand_user = models.CharField(max_length=30)
+
+
+    class Stat(models.TextChoices):
+        VERY_GOOD = 'A', _("Very Good")
+        GOOD = 'B', _("Good")
+        AVERAGE = 'C', _("Average")
+        POOR = 'D', _("Poor")
+        VERY_POOR = 'E', _("Very Poor")
+        NONE = '∅', _("None")
+        INFINITE = '∞', _("Infinite")
+        UNKNOWN = '?', _("Unknown")
+    
+
+    destructive_power_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+    speed_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+    range_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+    stamina_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+    precision_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+    development_potential_stat = models.CharField(
+        max_length = 1,
+        choices = Stat.choices,
+        default = Stat.UNKNOWN,
+    )
+
+    def __str__(self):
+        return f'{self.stand_name}({self.stand_user})\
+            [{self.destructive_power_stat},\
+            {self.speed_stat},\
+            {self.range_stat},\
+            {self.stamina_stat},\
+            {self.precision_stat},\
+            {self.development_potential_stat}]'
+    
