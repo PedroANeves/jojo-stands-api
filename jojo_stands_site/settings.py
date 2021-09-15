9f79196f89adb7923cf8958c4ced6fcf53212912
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-15 19:00:46 -0300

    changed SECRET_KEY from txt file to env variable

--- jojo_stands_site/settings.py
+++ jojo_stands_site/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-with open('./secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+# with open('./secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
