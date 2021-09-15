977b1405e58286bc3796cd7987bdd7c43007ca42
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-15 17:54:24 -0300

    changed field names in StandSerializers class

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,25 @@ from rest_framework import serializers
 from api.models import Stand
 
 class StandSerializer(serializers.HyperlinkedModelSerializer):
+    STAND = serializers.CharField(source='stand_name')
+    PWR = serializers.CharField(source='destructive_power_stat')
+    SPD = serializers.CharField(source='speed_stat')
+    RNG = serializers.CharField(source='range_stat')
+    STA = serializers.CharField(source='stamina_stat')
+    PRC = serializers.CharField(source='precision_stat')
+    DEV = serializers.CharField(source='development_potential_stat')
+
     class Meta:
         model = Stand
-        fields = ['id', 'url', 'stand_name', 'stand_user']
+        fields = [
+        'id',
+        'url',
+        'STAND',
+        # 'stand_user',
+        'PWR',
+        'SPD',
+        'RNG',
+        'STA',
+        'PRC',
+        'DEV',
+        ]
