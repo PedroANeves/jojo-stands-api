f93f1bb1e3681551383f6137c098307f9a13b0ad
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-15 17:28:11 -0300

    added StandSerializer to views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Stand
+from .serializers import StandSerializer
+
+
+class StandViewSet(viewsets.ModelViewSet):
+    queryset = Stand.objects.all()
+    serializer_class = StandSerializer
+
