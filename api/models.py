50e5afd94b0d06db4f504d46a9b15d3ca006fd79
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-11-07 12:00:55 -0300

    added stand_part to Stand

--- api/models.py
+++ api/models.py
@@ -4,18 +4,37 @@ from django.utils.translation import gettext_lazy as _
 
 class Stand(models.Model):
     stand_name = models.CharField(max_length=30)
-    stand_user = models.CharField(max_length=30)
+    stand_user = models.CharField(max_length=50)
+    
+
+    class Part(models.IntegerChoices):
+        PART_UNKNOWN = 0, _('No part')
+        REAL_LIFE = 1, _('Real Life Stand')
+        PURPLE_HAZE_LN = 2, _('Purple Haze Feedback Light Novel')
+        PART_THREE = 3, _('Part 3, Stardust Crusaders.')
+        PART_FOUR = 4, _('Part 4, Diamond Is Unbreakable.')
+        PART_FIVE = 5, _('Part 5, Golden Wind.')
+        PART_SIX = 6, _('Part 6, Stone Ocean.')
+        PART_SEVEN = 7, _('Part 7, Steel Ball Run.')
+        PART_EIGHT = 8, _('Part 8, JoJolion.')
+        __empty__ = _('Part unknown')
+    
+
+    stand_part = models.IntegerField(
+        choices = Part.choices,
+        default = Part.PART_UNKNOWN
+    )
 
 
     class Stat(models.TextChoices):
-        VERY_GOOD = 'A', _("Very Good")
-        GOOD = 'B', _("Good")
-        AVERAGE = 'C', _("Average")
-        POOR = 'D', _("Poor")
-        VERY_POOR = 'E', _("Very Poor")
-        NONE = '∅', _("None")
-        INFINITE = '∞', _("Infinite")
-        UNKNOWN = '?', _("Unknown")
+        VERY_GOOD = 'A', _("(A) Very Good")
+        GOOD = 'B', _("(B) Good")
+        AVERAGE = 'C', _("(C) Average")
+        POOR = 'D', _("(D) Poor")
+        VERY_POOR = 'E', _("(E) Very Poor")
+        NONE = '∅', _("(∅) None")
+        INFINITE = '∞', _("(∞) Infinite")
+        UNKNOWN = '?', _("(?) Unknown")
     
 
     destructive_power_stat = models.CharField(
