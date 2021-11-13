ddde26517244dd970fe4858c57709cf5c9774020
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-11-13 20:58:16 -0300

    added the nested AtrubuteSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,16 +2,32 @@ from rest_framework import serializers
 
 from api.models import Stand
 
-class StandSerializer(serializers.HyperlinkedModelSerializer):
-    STAND = serializers.CharField(source='stand_name')
-    # USER = serializers.CharField(source='stand_user')
-    PART = serializers.IntegerField(source='stand_part')
+
+class AtributeSerializer(serializers.HyperlinkedModelSerializer):
     PWR = serializers.CharField(source='destructive_power_stat')
     SPD = serializers.CharField(source='speed_stat')
     RNG = serializers.CharField(source='range_stat')
     STA = serializers.CharField(source='stamina_stat')
     PRC = serializers.CharField(source='precision_stat')
     DEV = serializers.CharField(source='development_potential_stat')
+    
+    class Meta:
+        model = Stand
+        fields = [
+        'PWR',
+        'SPD',
+        'RNG',
+        'STA',
+        'PRC',
+        'DEV',
+        ]
+
+
+class StandSerializer(serializers.HyperlinkedModelSerializer):
+    STAND = serializers.CharField(source='stand_name')
+    # USER = serializers.CharField(source='stand_user')
+    PART = serializers.IntegerField(source='stand_part')
+    atributes = AtributeSerializer(source='*')
 
     class Meta:
         model = Stand
@@ -21,10 +37,5 @@ class StandSerializer(serializers.HyperlinkedModelSerializer):
         'STAND',
         # 'USER',
         'PART',
-        'PWR',
-        'SPD',
-        'RNG',
-        'STA',
-        'PRC',
-        'DEV',
+        'atributes',
         ]
